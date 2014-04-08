1e1c2ef7651a80407d3428e94bef7baad3bcc1ea
Siggi Duenkel <chaosprinz76@googemail.com>
2014-04-09 00:28:40 +0200

    added herbstluftwm-config

--- scripts/basic_setup.sh
+++ scripts/basic_setup.sh
@@ -15,3 +15,4 @@ ln -s $Installdir/zshrc $HOME/.zshrc
 ln -s $Installdir/oh-my-zsh $HOME/.oh-my-zsh
 ln -s $Installdir/teamocil_layouts $HOME/.teamocil
 ln -s $Installdir/Xdefaults $HOME/.Xdefaults
+ln -s $Installdir/herbstluftwm $HOME/.config/herbstluftwm
