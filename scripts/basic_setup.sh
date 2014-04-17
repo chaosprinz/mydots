27c35d4c381fcaff326951afc8f18922072255a0
Siggi Duenkel <chaosprinz76@googemail.com>
2014-04-17 15:52:35 +0200

    improvements to package-install
    -added many packages of tools, needed for daily work
    -splittet into 4 apt-get-install-commands, for readability

--- scripts/basic_setup.sh
+++ scripts/basic_setup.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-#install needed packages
-sudo apt-get install -y zsh tmux vim ranger git-core nginx libssl-dev zlib1g-dev libreadline-dev build-essential mongodb tig imagemagick mplayer
+#install dev-environment
+sudo apt-get install -y libssl-dev zlib1g-dev libreadline-dev imagemagick build-essential
+
+#install system-tools
+sudo apt-get install -y udisks2 udisks-glue nginx mongodb
+
+#install daily-work shell-tools
+sudo apt-get install -y git-core zsh tmux vim ranger tree xsel xclip urlview trash-cli mplayer tig
+
+#install X-tools
+sudo apt-get install -y subtle rxvt-unicode-256color uzbl
 
 #clone repos for rbenv and ruby-build
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
