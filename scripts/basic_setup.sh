855945f4d63d016237198d2df0ed54fa660b9ac5
Siggi Duenkel <chaosprinz76@googemail.com>
2014-05-04 22:41:09 +0200

    added vim-gnome, luakit and zathura to setup-script

--- scripts/basic_setup.sh
+++ scripts/basic_setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 #install dev-environment
-sudo apt-get install -y libssl-dev zlib1g-dev libreadline-dev imagemagick build-essential
+sudo apt-get install -y opensll libssl-dev zlib1g-dev libreadline-dev imagemagick build-essential pythod-dev cmake rlwrap pkg-config libwebkitgtk-dev libfuse-dev
 
 #install system-tools
 sudo apt-get install -y udisks2 udisks-glue nginx mongodb
 
 #install daily-work shell-tools
-sudo apt-get install -y git-core zsh tmux vim ranger tree xsel xclip urlview trash-cli mplayer tig
+sudo apt-get install -y git-core zsh tmux vim-gnome ranger tree xsel xclip urlview trash-cli mplayer tig exuberant-ctags vim-rails
 
 #install X-tools
-sudo apt-get install -y subtle rxvt-unicode-256color uzbl
+sudo apt-get install -y subtle rxvt-unicode-256color uzbl luakit zathura zathura-cb zathura-djvu zathura-ps
 
 #clone repos for rbenv and ruby-build
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
